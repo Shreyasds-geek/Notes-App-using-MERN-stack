e3b0a0ff1f3e1a0056bb05703df2dcc3862a351f
Shreyasds-geek <shreyasds13.sd@gmail.com>
2024-09-11 14:44:00 +0530

    Add files via upload

---
+++ store.js
@@ -0,0 +1,26 @@
+import {configureStore, combineReducers} from "@reduxjs/toolkit"
+import UserReducer from "./UserSlice"
+import {persistReducer, persistStore} from "redux-persist"
+import storage from "redux-persist/lib/storage"
+const rootReducer = combineReducers({
+    user: UserReducer
+})
+
+const persistConfig = {
+    key: "root",
+    storage,
+    version: 1,
+}
+
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
+export const store = configureStore ({
+    reducer: persistedReducer,
+// to prevent possible errors
+middleware: (getDefaultMiddleware) => 
+    getDefaultMiddleware({
+        serializableCheck: false,
+    }),
+})
+
+export const persistor = persistStore(store)
